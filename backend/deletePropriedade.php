5e3db82b3e2615d7d2eb89ad73c3611fdfb678d6
José Gabriel Siqueira Braga <jos.gabriel210@gmail.com>
2026-01-14 13:38:00 -0400

    att

--- backend/deletePropriedade.php
+++ backend/deletePropriedade.php
@@ -1,9 +1,17 @@
 <?php
-    include __DIR__ . '../backend/conexao.php';
-    $id = base64_decode(filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING));
+    include __DIR__ . '/conexao.php';
+    $id = base64_decode(filter_input(INPUT_GET, 'id', FILTER_SANITIZE_SPECIAL_CHARS));
     // echo "UPDATE propriedades SET status = 0 WHERE id = $id; UPDATE talhao SET status= 0 WHERE id_propriedade = $id; UPDATE maquina SET status= 0 WHERE id_propriedade = $id";
-    $sqlInsert = $conn->prepare("UPDATE propriedades SET status = 0 WHERE id = :id; UPDATE talhao SET status= 0 WHERE id_propriedade = :id; UPDATE maquina SET status= 0 WHERE id_propriedade = :id");
-    $sqlInsert->bindParam(':id', $id);
-    $sqlInsert->execute();
+    $sqlProp = $conn->prepare("UPDATE propriedades SET status = 0 WHERE id = :id");
+    $sqlProp->bindParam(':id', $id);
+    $sqlProp->execute();
+
+    $sqlTalhao = $conn->prepare("UPDATE talhao SET status = 0 WHERE id_propriedade = :id");
+    $sqlTalhao->bindParam(':id', $id);
+    $sqlTalhao->execute();
+
+    $sqlMaq = $conn->prepare("UPDATE maquina SET status = 0 WHERE id_propriedade = :id");
+    $sqlMaq->bindParam(':id', $id);
+    $sqlMaq->execute();
     header("Location: ../propriedades.php");
 ?>
