92ed9527677dea80a4858e8d816635f1e9f10368
José Gabriel Siqueira Braga <jos.gabriel210@gmail.com>
2026-01-14 08:18:16 -0400

    att

--- backend/salvarSafra.php
+++ backend/salvarSafra.php
@@ -8,7 +8,6 @@
     $dataInicio = filter_input(INPUT_POST, 'dataInicio', FILTER_SANITIZE_SPECIAL_CHARS);
     $dataFinal = filter_input(INPUT_POST, 'dataFinal', FILTER_SANITIZE_SPECIAL_CHARS);
     $cultura = filter_input(INPUT_POST, 'cultura', FILTER_SANITIZE_SPECIAL_CHARS);
-
     
     $sqlInsert = $conn->prepare("INSERT INTO safra(id_cultura, descricao, data_inicio, data_fim) VALUES (:cultura, :descricaoSafra, :dataInicio, :dataFinal)");
     $sqlInsert->bindParam(':descricaoSafra', $descricaoSafra);       
@@ -17,17 +16,11 @@
     $sqlInsert->bindParam(':cultura', $cultura);
     $sqlInsert->execute();
 
-
-
     $idUsuario = 123;
     $acao = "Cadastrou a safra ".$descricaoSafra;
-    $ip = $_SERVER['IP'];
+    $ip = $_SERVER['REMOTE_ADDR'];
 
     $sqlInsert3 = $conn->query("INSERT INTO registro_acao(id_usuario, acao, data, ip) VALUES ('$idUsuario', '$acao', '$dataCriacao', '$ip')");
 
-
     header("Location: ../safras.php?status=created_ok")
-
-    
-    
 ?>
