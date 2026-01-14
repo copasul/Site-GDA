a6939c3f6726f71c9324ab72fcc616d05fa02dd8
José Gabriel Siqueira Braga <jos.gabriel210@gmail.com>
2026-01-14 14:15:11 -0400

    att

--- dispositivos-ativos.php
+++ dispositivos-ativos.php
@@ -2,9 +2,17 @@
     include __DIR__ . '/backend/conexao.php';
     include __DIR__ . '/backend/verificaLog.php';
 
+    $titulo = $titulo ?? 'Copasul';
+
     $idUsuario = $User['id'];
-    $sqlBusca = $conn->query("SELECT * FROM login_registro WHERE id_usuario = $idUsuario AND token != '' ORDER BY data_hora");
-    // echo "SELECT * FROM login_registro WHERE id_usuario = $idUsuario AND token != '' ORDER BY data_hora";
+    
+    $sqlBusca = $conn->prepare("
+        SELECT * FROM login_registro 
+        WHERE id_usuario = :idUsuario 
+        AND token != '' 
+        ORDER BY data_hora
+    ");
+    $sqlBusca->execute([':idUsuario' => $idUsuario]);
    
 ?>
 <!DOCTYPE html>
@@ -12,44 +20,33 @@
     <head>
         <title><?php echo $titulo?> - Dispositivos</title>
 
-        <!-- Inclusão do arquivo 'head', contendo informações gerais -->
         <?php include __DIR__ . '/head.php'; ?>
         <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     </head>
 
 <body id="page-top">
-    <!-- Page Wrapper -->
     <div id="wrapper">
-        <!-- Sidebar -->
         <?php include __DIR__ . '/nav-bar.php'; ?>
-        <!-- End of Sidebar -->
-
-        <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
             <div id="content">
 
-                <!-- Topbar -->
                 <?php include __DIR__ . '/top-bar.php'; ?>
-                <!-- End of Topbar -->
-
-                <!-- Begin Page Content -->
                 <div class="container-fluid">
 
-                    <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dispositivos Ativos</h1>
                     </div>
                     <?php 
                         while( $dados = $sqlBusca->fetch(PDO::FETCH_ASSOC)){
+                            $tokenAtual = $tokenAcess ?? ''; 
                     ?>
                     <div class="row">
                         <div class="col-md-12">
                             <div class="card shadow mb-4">
                                 <?php
-                                    if($tokenAcess == $dados['token']){
-                                        
+                                    if($tokenAtual == $dados['token']){
+                                            
                                 ?>
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Este dispositivo</h6>
@@ -59,45 +56,47 @@
                                 ?>
                                 <div class="card-body">
                                    <div class="row">
-                                        <div class="col-md-2">
-                                            <div class="card shadow mb-4">
-                                                <div class="card-body">
-                                                    <img src="img/<?php if($dados['tipo_acesso'] == 'web'){ echo "notebook.png";}else{echo "smartphone.png";}?>" alt="" width="100%">
+                                            <div class="col-md-2">
+                                                <div class="card shadow mb-4">
+                                                    <div class="card-body">
+                                                        <img src="img/<?php if($dados['tipo_acesso'] == 'web'){ echo "notebook.png";}else{echo "smartphone.png";}?>" alt="" width="100%">
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-md-9">
-                                            <div class="row">
-                                                <h6 class="h3 m-0 font-weight-bold text-gray"><?php if($dados['tipo_acesso'] == 'web'){ echo "Computador";}else{echo "Smartphone";}?></h6>
+                                            <div class="col-md-9">
+                                                <div class="row">
+                                                    <h6 class="h3 m-0 font-weight-bold text-gray"><?php if($dados['tipo_acesso'] == 'web'){ echo "Computador";}else{echo "Smartphone";}?></h6>
+                                                </div>
+                                                <?php
+                                                    if($dados['tipo_acesso']=='app'){
+                                                        $marca = $dados['marca_dispositivo'] ?? $dados['marca-dispositivo'] ?? '';
+                                                        $modelo = $dados['modelo_dispositivo'] ?? $dados['modelo-dispositivo'] ?? '';
+                                                        $versao = $dados['versao_dispositivo'] ?? $dados['versao-dispositivo'] ?? '';
+                                                ?>
+                                                <div class="row">
+                                                    <h6 class="m-0 text-gray"><?php echo $marca." ".$modelo." | Android ".$versao?></h6>
+                                                </div>
+                                                <?php
+                                                    }
+                                                ?>
+                                                <div class="row">
+                                                    <h6 class="m-0 text-gray">IP: <?php echo $dados['ip']?></h6>
+                                                </div>
+                                                <div class="row">
+                                                    <h6 class="m-0  text-gray">Data conexão:  <?php echo date('d/m/Y H:i', strtotime($dados['data_hora']))?> (horario de Brasília)</h6>
+                                                </div>
+                                                <br>
+                                                <?php
+                                                    if($tokenAtual <> $dados['token']){
+                                            
+                                                ?>
+                                                <div class="row">
+                                                    <button class="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm" onclick="location.href='backend/matarSessao.php?token=<?php echo $dados['token']?>'">Desconectar</button>
+                                                </div>
+                                                <?php
+                                                    }
+                                                ?>
                                             </div>
-                                            <?php
-                                                if($dados['tipo_acesso']=='app'){
-                                                    
-                                            ?>
-                                            <div class="row">
-                                                <h6 class="m-0 text-gray"><?php echo $dados['marca-dispositivo']." ".$dados['modelo-dispositivo']." | Android ".$dados['versao-dispositivo']?></h6>
-                                            </div>
-                                            <?php
-                                                }
-                                            ?>
-                                            <div class="row">
-                                                <h6 class="m-0 text-gray">IP: <?php echo $dados['ip']?></h6>
-                                            </div>
-                                            <div class="row">
-                                                <h6 class="m-0  text-gray">Data conexão:  <?php echo date('d/m/Y H:i', strtotime($dados['data_hora']))?> (horario de Brasília)</h6>
-                                            </div>
-                                            <br>
-                                            <?php
-                                                if($tokenAcess <> $dados['token']){
-                                        
-                                            ?>
-                                            <div class="row">
-                                                <button class="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm" onclick="location.href='backend/matarSessao.php?token=<?php echo $dados['token']?>'">Desconectar</button>
-                                            </div>
-                                            <?php
-                                                }
-                                            ?>
-                                        </div>
                                    </div>
                                 </div>
                             </div>
@@ -108,12 +107,7 @@
                     ?>
                     
                 </div>
-                <!-- /.container-fluid -->
-
-            </div>
-            <!-- End of Main Content -->
-
-            <!-- Footer -->
+                </div>
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
@@ -121,20 +115,12 @@
                     </div>
                 </div>
             </footer>
-            <!-- End of Footer -->
-
+            </div>
         </div>
-        <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
     <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
@@ -153,23 +139,18 @@
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-    <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
 
-    <!-- Page level plugins -->
     <script src="vendor/datatables/jquery.dataTables.min.js"></script>
     <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
-    <!-- Page level custom scripts -->
     <script src="js/demo/datatables-demo.js"></script>
-   
+    
 </body>
 
 </html>
