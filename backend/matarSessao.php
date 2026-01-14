70b830d746c9382fa3928085fe30ee5f48582bcb
José Gabriel Siqueira Braga <jos.gabriel210@gmail.com>
2026-01-14 14:24:41 -0400

    att

--- backend/matarSessao.php
+++ backend/matarSessao.php
@@ -1,9 +1,14 @@
 <?php
-include __DIR__ . '../backend/conexao.php';
+    include __DIR__ . '/conexao.php';
+
     if(!empty($_GET['token'])){
         $token = $_GET['token'];
-        $sql = $conn->query("UPDATE login_registro SET token=NULL WHERE token = '$token'");
+        
+        $sql = $conn->prepare("UPDATE login_registro SET token=NULL WHERE token = :token");
+        $sql->execute([':token' => $token]);
+        
         header('location: ../dispositivos-ativos.php');
+        exit;
     }
 
 ?>
