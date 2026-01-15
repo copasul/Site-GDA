38bfbcf45ffcfee82f1d4285046ae03679ffbb15
José Gabriel Siqueira Braga <jos.gabriel210@gmail.com>
2026-01-15 08:20:29 -0400

    att

--- top-bar.php
+++ top-bar.php
@@ -39,7 +39,7 @@
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $User['nome']?></span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $nomeUsuario['nome']?></span>
                 <img class="img-profile rounded-circle" src="<?php echo $urlBase?>img/undraw_profile.svg">
             </a>
             <!-- Dropdown - User Information -->
