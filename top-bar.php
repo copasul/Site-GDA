e900af472945af2700c69ce5069338323efed42d
José Gabriel Siqueira Braga <jos.gabriel210@gmail.com>
2026-01-15 08:23:27 -0400

    att

--- top-bar.php
+++ top-bar.php
@@ -1,5 +1,9 @@
 <?php
-    $nomeUsuario = $User['nome'] ?? 'Usuário';
+    if (!isset($User)) {
+        $User = [];
+    }
+
+    $User['nome'] = $User['nome'] ?? 'Usuário';
     $urlBase = $urlBase ?? ''; 
 ?>
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
@@ -39,7 +43,7 @@
         <!-- Nav Item - User Information -->
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $nomeUsuario['nome']?></span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo $User['nome']?></span>
                 <img class="img-profile rounded-circle" src="<?php echo $urlBase?>img/undraw_profile.svg">
             </a>
             <!-- Dropdown - User Information -->
