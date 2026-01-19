1ccd56dcd744bccf618ecb676cb79e6ae8a094e8
José Gabriel Siqueira Braga <jos.gabriel210@gmail.com>
2026-01-19 10:18:16 -0400

    att

--- APP/dadosPropriedade.php
+++ APP/dadosPropriedade.php
@@ -49,49 +49,43 @@
             $listaPro[] = base64_encode($listPropriedade['id']."-".$listPropriedade['nome']);
             $lisPro[base64_encode($listPropriedade['id'])] = base64_encode($listPropriedade['nome']);
             
-            
             $idPropriedade = $listPropriedade['id'];
+            
+            // ... (MANTENHA SUAS QUERIES SQL AQUI: $sqlMaquina, $sqlTalhao, etc) ...
             $sqlMaquina = $conn->query("SELECT id, nome, modelo, id_propriedade FROM maquina WHERE id_propriedade = '$idPropriedade' AND status = 1");
             $sqlTalhao = $conn->query("SELECT id, nome, id_propriedade FROM talhao WHERE id_propriedade = '$idPropriedade' AND status = 1");
-            
-            
             $sqlPerdaTalhao = $conn->query("SELECT * FROM (SELECT id_talhao, AVG(perda_total) AS medidatalhao FROM $nomeTabela WHERE id_propriedade = '$idPropriedade' AND id_safra = '$safra' AND perda_total > 0 GROUP BY id_talhao ORDER BY medidatalhao DESC LIMIT 5) as A INNER JOIN Talhao ON A.id_talhao = Talhao.id");
             $sqlPerdaMaquina = $conn->query("SELECT A.MediaMaquina, Maquina.nome, Maquina.modelo FROM (SELECT id_maquina, AVG(perda_total) AS MediaMaquina FROM $nomeTabela WHERE id_propriedade = '$idPropriedade' AND id_safra = '$safra' AND perda_total > 0 GROUP BY id_maquina ORDER BY MediaMaquina DESC LIMIT 5) as A INNER JOIN Maquina ON A.id_maquina = Maquina.id");
-            
             $sqlPerdaTalhaoMedia = $conn->query("SELECT * FROM (SELECT id_talhao, AVG(perda_total) AS medidatalhao FROM $nomeTabela WHERE id_propriedade = '$idPropriedade' AND id_safra = '$safra' AND perda_total > 0 GROUP BY id_talhao ORDER BY medidatalhao DESC) as A INNER JOIN Talhao ON A.id_talhao = Talhao.id");
-            
+
+
             //lista as maquinas
-            unset($listaMaqUni);
+            $listaMaqUni = []; // Zera o array localmente
             $listaMaqUni[base64_encode("--")] = base64_encode("Selecione...");
             while($listMaquina = $sqlMaquina->fetch(PDO::FETCH_ASSOC)){
                 $listaMaqUni[base64_encode($listMaquina['id'])] = base64_encode($listMaquina['nome']."-".$listMaquina['modelo']);
-                
             }
             
             //lista os talhoes
-            unset($listaTalUni);
+            $listaTalUni = []; // Zera o array localmente
             $listaTalUni[base64_encode("--")] = base64_encode("Selecione...");
             while($listTalhao = $sqlTalhao->fetch(PDO::FETCH_ASSOC)){
                 $listaTalUni[base64_encode($listTalhao['id'])] = base64_encode($listTalhao['nome']);
             }
             
-            
             //lista perdas maquinas
-            unset($listaMaqPerda);
+            $listaMaqPerda = []; // Zera o array localmente
             while($listPerMaq = $sqlPerdaMaquina->fetch(PDO::FETCH_ASSOC)){
                 $listaMaqPerda[base64_encode($listPerMaq['nome'])] = base64_encode($listPerMaq['MediaMaquina']);
-                
             }
             
             //lista perdas talhoes
-            unset($listaTaPerda);
-            unset($listaTaPerdaLegenda);
+            $listaTaPerda = []; // Zera o array localmente
             while($listPerTa = $sqlPerdaTalhao->fetch(PDO::FETCH_ASSOC)){
                 $listaTaPerda[base64_encode($listPerTa['nome'])] = base64_encode($listPerTa['medidatalhao']);
-
             }
             
-            //calcular perda media por propriedade
+            //calcular perda media
             $perdaAculumada = 0;
             $areaAcumulada = 0;
             while($listPerda= $sqlPerdaTalhaoMedia->fetch(PDO::FETCH_ASSOC)){
@@ -104,19 +98,24 @@
                 $mediaPonderadaTalhao = 0;
             }
             
+            // Popula os arrays globais
             $listaMaq[base64_encode($idPropriedade)] = $listaMaqUni;
             $listaTal[base64_encode($idPropriedade)] = $listaTalUni;
             $listaPerdaPropriedade[base64_encode($idPropriedade)] = base64_encode(number_format($mediaPonderadaTalhao, 2, ',', '.'));
             
-            
+            // Verifica se tem dados antes de adicionar ao array global
             if(!empty($listaMaqPerda)){
                 $gra_maq_perda[base64_encode($idPropriedade)] = $listaMaqPerda;
+            } else {
+                 // IMPORTANTE: Enviar vazio ao invés de indefinido para o app não quebrar
+                 $gra_maq_perda[base64_encode($idPropriedade)] = []; 
             }
             
             if(!empty($listaTaPerda)){
                 $gra_ta_perda[base64_encode($idPropriedade)] = $listaTaPerda;
+            } else {
+                 $gra_ta_perda[base64_encode($idPropriedade)] = [];
             }
-            
         }
         
         
