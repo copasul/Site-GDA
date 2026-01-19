ab9c8542d43697ac12547320553f0df0f78243e8
José Gabriel Siqueira Braga <jos.gabriel210@gmail.com>
2026-01-19 09:23:35 -0400

    att

--- APP/encerrarSessao.php
+++ APP/encerrarSessao.php
@@ -1,6 +1,7 @@
 <?php
 include __DIR__ . '/../backend/conexao.php';
-$token = $_POST['token'];
+
+$token = $_POST['token'] ?? ''; 
 
 $sql = $conn->query("UPDATE login_registro SET token=NULL WHERE token = '$token'");
-
+?>
